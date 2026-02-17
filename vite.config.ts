7eb13e7e3c9d0f4a1bc6c7e5cc8cb25b7db9b470
mrjimin <aa090402@naver.com>
2026-02-17 09:45:00 +0900

    change .env

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, './src'),
-      "@components": path.resolve(__dirname, './src/components'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, './src'),
+        "@components": path.resolve(__dirname, './src/components'),
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://api.listify.life/',
-        changeOrigin: true,
-        // rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  };
+});
